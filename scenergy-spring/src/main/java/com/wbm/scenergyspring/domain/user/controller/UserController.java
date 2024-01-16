5b85855e4db0da69f3b34e0045cd12dd5164070b
bluepeng9 <bluepeng9@gmail.com>
2024-01-16 17:13:12 +0900

    fix: request body 어노테이션 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/user/controller/UserController.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.wbm.scenergyspring.domain.user.controller.request.CreateUserRequest;
 import com.wbm.scenergyspring.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,9 @@ public class UserController {
     final UserService userService;
 
     @PostMapping
-    public void createUser(CreateUserRequest request) {
+    public void createUser(
+            @RequestBody CreateUserRequest request
+    ) {
         userService.createUser(request.toCreateUserCommand());
     }
 }
