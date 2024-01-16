0fb95bd68dbce80e601427563d72144660248cf7
bluepeng9 <bluepeng9@gmail.com>
2024-01-16 15:46:52 +0900

    feat: spring module 추가

---
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/ScenergySpringApplication.java
@@ -0,0 +1,13 @@
+package com.wbm.scenergyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenergySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScenergySpringApplication.class, args);
+    }
+
+}
---
+++ scenergy-spring/src/test/java/com/wbm/scenergyspring/ScenergySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbm.scenergyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenergySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
