c77a35622743f4d2793293b490c33fa368cff38d
bluepeng9 <bluepeng9@gmail.com>
2024-01-17 15:25:53 +0900

    feat: BaseEntity 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/user/entity/User.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/user/entity/User.java
@@ -1,5 +1,7 @@
 package com.wbm.scenergyspring.domain.user.entity;
 
+import com.wbm.scenergyspring.global.entity.BaseEntity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,21 +14,21 @@ import lombok.NoArgsConstructor;
 //user 는 예약어이므로 테이블 이름을 변경해야 함
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String email;
-    String password;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	String email;
+	String password;
 
-    public static User createNewUser(
-            String email,
-            String password
-    ) {
-        User user = new User();
-        user.email = email;
-        user.password = password;
-        return user;
-    }
+	public static User createNewUser(
+		String email,
+		String password
+	) {
+		User user = new User();
+		user.email = email;
+		user.password = password;
+		return user;
+	}
 }
---
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/global/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.wbm.scenergyspring.global.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+}
