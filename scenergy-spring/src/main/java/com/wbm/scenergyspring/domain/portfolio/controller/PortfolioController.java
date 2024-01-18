7192601cdd298d6d6b57851500442c8ff276bc1a
pedada <korea9300@naver.com>
2024-01-18 16:57:31 +0900

    feat: CreatePortfolioResponse, UpdatePortfolioResponse 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/portfolio/controller/PortfolioController.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/portfolio/controller/PortfolioController.java
@@ -2,9 +2,13 @@ package com.wbm.scenergyspring.domain.portfolio.controller;
 
 import com.wbm.scenergyspring.domain.portfolio.controller.request.CreatePortfolioRequest;
 import com.wbm.scenergyspring.domain.portfolio.controller.request.UpdatePortfolioRequest;
+import com.wbm.scenergyspring.domain.portfolio.controller.response.CreatePortfolioResponse;
+import com.wbm.scenergyspring.domain.portfolio.controller.response.UpdatePortfolioResponse;
 import com.wbm.scenergyspring.domain.portfolio.service.PortfolioService;
+import com.wbm.scenergyspring.domain.user.controller.response.CreateUserResponse;
 import com.wbm.scenergyspring.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +22,23 @@ public class PortfolioController {
     final PortfolioService portfolioService;
 
     @PostMapping("/create")
-    public ApiResponse<Boolean> createPortfolio(
+    public ResponseEntity<ApiResponse<CreatePortfolioResponse>> createPortfolio(
             @RequestBody CreatePortfolioRequest request
     ) {
-        portfolioService.createPortfolio(request.toCreatePortfolioCommand());
-        return ApiResponse.createSuccess(true);
+        Long portfolioId = portfolioService.createPortfolio(request.toCreatePortfolioCommand());
+
+        CreatePortfolioResponse createPortfolioResponse = new CreatePortfolioResponse();
+        createPortfolioResponse.setPortfolioId(portfolioId);
+
+        return ResponseEntity.ok(ApiResponse.createSuccess(createPortfolioResponse));
     }
     @PostMapping("/update")
-    public ApiResponse<Boolean> updatePortfolio(
+    public ResponseEntity<ApiResponse<UpdatePortfolioResponse>> updatePortfolio(
             @RequestBody UpdatePortfolioRequest request
     ) {
-        portfolioService.updatePortfolio(request.toUpdatePortfolioCommand());
-        return ApiResponse.createSuccess(true);
+        Long portfolioId = portfolioService.updatePortfolio(request.toUpdatePortfolioCommand());
+        UpdatePortfolioResponse updatePortfolioResponse = new UpdatePortfolioResponse();
+        updatePortfolioResponse.setPortfolioId(portfolioId);
+        return ResponseEntity.ok(ApiResponse.createSuccess(updatePortfolioResponse));
     }
 }
