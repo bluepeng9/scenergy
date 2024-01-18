126bb2b11a71c44a0724b05090559e53f01864d0
bluepeng9 <bluepeng9@gmail.com>
2024-01-18 16:36:41 +0900

    feat: generated value strategy 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/follow/entity/Follow.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/follow/entity/Follow.java
@@ -5,6 +5,8 @@ import com.wbm.scenergyspring.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
@@ -12,6 +14,7 @@ import jakarta.persistence.ManyToOne;
 public class Follow {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "follow_id")
 	Long id;
 
