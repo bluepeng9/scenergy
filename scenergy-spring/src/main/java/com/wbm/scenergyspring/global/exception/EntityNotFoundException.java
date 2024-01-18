8357b4fd1892c561e916bfa82eacb89de6f1d3ce
bluepeng9 <bluepeng9@gmail.com>
2024-01-18 16:49:50 +0900

    feat: EntityNotFoundException 추가

---
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/global/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.wbm.scenergyspring.global.exception;
+
+public class EntityNotFoundException extends BusinessException {
+	public EntityNotFoundException(String message) {
+		super(message);
+	}
+}
