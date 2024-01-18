f64dedaac0d5c9dee060ad9880dd1ea7f74f0f09
pedada <korea9300@naver.com>
2024-01-18 16:27:20 +0900

    feat: portfolio service update 기능 추가
    create 메서드 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/portfolio/service/PortfolioService.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/portfolio/service/PortfolioService.java
@@ -3,9 +3,12 @@ package com.wbm.scenergyspring.domain.portfolio.service;
 import com.wbm.scenergyspring.domain.portfolio.Portfolio;
 import com.wbm.scenergyspring.domain.portfolio.repository.PortfolioRepository;
 import com.wbm.scenergyspring.domain.portfolio.service.command.CreatePortfolioCommand;
+import com.wbm.scenergyspring.domain.portfolio.service.command.UpdatePortfolioCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,14 +19,13 @@ public class PortfolioService {
 
     @Transactional(readOnly = false)
     public Long createPortfolio(CreatePortfolioCommand command) {
-        Portfolio newPortfolio = Portfolio.createNewPortfolio(
-                command.getUserId(),
-                command.getDescription(),
-                command.getEducations(),
-                command.getExperiences(),
-                command.getHonors(),
-                command.getEtcs()
-        );
+        Portfolio newPortfolio = Portfolio.createNewPortfolio(command.getUserId());
         return portfolioRepository.save(newPortfolio).getId();
+    }
+    @Transactional(readOnly = false)
+    public Long updatePortfolio(UpdatePortfolioCommand command) {
+        Portfolio portfolio = portfolioRepository.findById(command.getPortfolioId()).get();
+        portfolio.updatePortfolio(portfolio, command);
+        return command.getPortfolioId();
     }
 }
