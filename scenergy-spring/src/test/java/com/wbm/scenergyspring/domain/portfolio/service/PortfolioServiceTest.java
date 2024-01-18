c0c5f3a5f6ed1d5910ec92a3bb08b6dfd3ed2b1e
pedada <korea9300@naver.com>
2024-01-18 16:33:09 +0900

    test: portfolio update test 추가

--- scenergy-spring/src/test/java/com/wbm/scenergyspring/domain/portfolio/service/PortfolioServiceTest.java
+++ scenergy-spring/src/test/java/com/wbm/scenergyspring/domain/portfolio/service/PortfolioServiceTest.java
@@ -3,6 +3,7 @@ package com.wbm.scenergyspring.domain.portfolio.service;
 import com.wbm.scenergyspring.domain.portfolio.*;
 import com.wbm.scenergyspring.domain.portfolio.repository.PortfolioRepository;
 import com.wbm.scenergyspring.domain.portfolio.service.command.CreatePortfolioCommand;
+import com.wbm.scenergyspring.domain.portfolio.service.command.UpdatePortfolioCommand;
 import com.wbm.scenergyspring.domain.user.service.command.CreateUserCommand;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,32 +34,41 @@ class PortfolioServiceTest {
         //then
         assertTrue(portfolioRepository.findById(1L).isPresent());
     }
-//    void createPortfolio(){
-//        //given
-//        List<Education> educations = new ArrayList<>();
-//        List<PortfolioEtc> etcs = new ArrayList<>();
-//        List<Experience> experiences = new ArrayList<>();
-//        List<Honor> honors = new ArrayList<>();
-//
-//        educations.add( Education.createNewEducation("inst", "degree", "major"));
-//        etcs.add(PortfolioEtc.createNewPortfolioEtc("title", "desc"));
-//        experiences.add(Experience.createNewExperience("company","position"));
-//        honors.add(Honor.createNewHonor("title", "orgam"));
-//
-//        CreatePortfolioCommand command = CreatePortfolioCommand.builder()
-//                .userId(1)
-//                .description("port desc")
-//                .educations(educations)
-//                .etcs(etcs)
-//                .experiences(experiences)
-//                .honors(honors)
-//                .build();
-//
-//        //when
-//        portfolioService.createPortfolio(command);
-//
-//        //then
-//        Portfolio portfolio = portfolioRepository.findById(1L).get();
-//        assertEquals(portfolio.getEducations().get(0).getInstitution(), command.getEducations().get(0).getInstitution());
-//    }
+    @Test
+    @Transactional
+    void updatePortfolio(){
+        //given
+        CreatePortfolioCommand createCommand = CreatePortfolioCommand.builder()
+                .userId(1)
+                .build();
+        Long portfolioId = portfolioService.createPortfolio(createCommand);
+
+        List<Education> educations = new ArrayList<>();
+        List<PortfolioEtc> etcs = new ArrayList<>();
+        List<Experience> experiences = new ArrayList<>();
+        List<Honor> honors = new ArrayList<>();
+
+        educations.add( Education.createNewEducation("inst", "degree", "major"));
+        etcs.add(PortfolioEtc.createNewPortfolioEtc("title", "desc"));
+        experiences.add(Experience.createNewExperience("company","position"));
+        honors.add(Honor.createNewHonor("title", "orgam"));
+
+        UpdatePortfolioCommand updateCommand = UpdatePortfolioCommand.builder()
+                .portfolioId(portfolioId)
+                .userId(2)
+                .description("port desc")
+                .educations(educations)
+                .etcs(etcs)
+                .experiences(experiences)
+                .honors(honors)
+                .build();
+
+        //when
+        Portfolio beforePortfolio = portfolioRepository.findById(portfolioId).get();
+        beforePortfolio.updatePortfolio(beforePortfolio, updateCommand);
+
+        //then
+        Portfolio afterPortfolio = portfolioRepository.findById(portfolioId).get();
+        assertEquals(afterPortfolio.getEducations().get(0).getInstitution(), updateCommand.getEducations().get(0).getInstitution());
+    }
 }
