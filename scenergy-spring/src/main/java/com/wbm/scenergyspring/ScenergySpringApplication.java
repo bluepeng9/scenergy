fd9a35092ba9c5e38d7f5300b4542f9de172e188
imin <lllgee@naver.com>
2024-01-19 12:39:10 +0900

    feat: ScenergySpringApplication @ConfigurationPropertiesScan 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/ScenergySpringApplication.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/ScenergySpringApplication.java
@@ -1,11 +1,15 @@
 package com.wbm.scenergyspring;
 
+import com.wbm.scenergyspring.global.config.PathProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class ScenergySpringApplication {
 
     public static void main(String[] args) {
