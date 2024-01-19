e66b995ae09a6b7d016ba4c5df281c4f62c07939
imin <lllgee@naver.com>
2024-01-19 12:44:00 +0900

    feat: Video 추가

---
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/entity/Video.java
@@ -0,0 +1,38 @@
+package com.wbm.scenergyspring.domain.post.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String videoUrl;
+
+    String thumbnail;
+
+    String musicTitle;
+
+    String artist;
+
+    public static Video createVideo(
+        String videoUrl,
+        String thumbnail,
+        String musicTitle,
+        String artist
+    ){
+        Video video = new Video();
+        video.videoUrl = videoUrl;
+        video.thumbnail = thumbnail;
+        video.musicTitle = musicTitle;
+        video.artist = artist;
+
+        return video;
+    }
+
+}
