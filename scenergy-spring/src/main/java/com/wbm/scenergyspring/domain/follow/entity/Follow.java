7b1b9691a6add0bb5778aae1058376bf37538675
bluepeng9 <bluepeng9@gmail.com>
2024-01-19 10:30:54 +0900

    feat: follow getter 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/follow/entity/Follow.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/follow/entity/Follow.java
@@ -9,8 +9,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Follow {
 
 	@Id
@@ -23,4 +25,15 @@ public class Follow {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	User to;
+
+	public static Follow createFollow(
+		User from,
+		User to
+	) {
+		Follow follow = new Follow();
+		follow.from = from;
+		follow.to = to;
+		return follow;
+	}
+
 }
