eceb510bad8ce5e78ed781d0582487573759b8bf
pedada <korea9300@naver.com>
2024-01-19 10:48:12 +0900

    test: portfolio delete test 추가

--- scenergy-spring/src/test/java/com/wbm/scenergyspring/domain/portfolio/service/PortfolioServiceTest.java
+++ scenergy-spring/src/test/java/com/wbm/scenergyspring/domain/portfolio/service/PortfolioServiceTest.java
@@ -3,7 +3,9 @@ package com.wbm.scenergyspring.domain.portfolio.service;
 import com.wbm.scenergyspring.domain.portfolio.entity.*;
 import com.wbm.scenergyspring.domain.portfolio.repository.PortfolioRepository;
 import com.wbm.scenergyspring.domain.portfolio.service.command.CreatePortfolioCommand;
+import com.wbm.scenergyspring.domain.portfolio.service.command.DeletePortfolioCommand;
 import com.wbm.scenergyspring.domain.portfolio.service.command.UpdatePortfolioCommand;
+import com.wbm.scenergyspring.global.exception.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -74,5 +76,27 @@ class PortfolioServiceTest {
         //then
         Portfolio afterPortfolio = portfolioRepository.findById(portfolioId).get();
         assertEquals(afterPortfolio.getEducations().get(0).getInstitution(), updateCommand.getEducations().get(0).getInstitution());
+    }
+    @Test
+    void deletePortfolio(){
+        //given
+        CreatePortfolioCommand createCommand = CreatePortfolioCommand.builder()
+                .userId(1)
+                .build();
+        Long portfolioId = portfolioService.createPortfolio(createCommand);
+
+        DeletePortfolioCommand deletePortfolioCommand = DeletePortfolioCommand.builder()
+                .portfolioId(portfolioId)
+                .userId(1)
+                .build();
+
+        //when
+        Long deletedPortfolioId = portfolioService.deletePortfolio(deletePortfolioCommand);
+
+        //then
+        assertThrows(EntityNotFoundException.class, ()->{
+            portfolioRepository.findById(portfolioId)
+                    .orElseThrow(()-> new EntityNotFoundException("DB에 존재하지 않음"));
+        });
     }
 }
