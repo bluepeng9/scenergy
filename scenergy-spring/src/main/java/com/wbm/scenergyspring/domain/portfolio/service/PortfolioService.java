67678a4a14a7cdae63952d5aa65070564108446a
pedada <korea9300@naver.com>
2024-01-19 09:27:57 +0900

    refactor: updatePortfolio vailidation 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/portfolio/service/PortfolioService.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/portfolio/service/PortfolioService.java
@@ -4,9 +4,13 @@ import com.wbm.scenergyspring.domain.portfolio.entity.Portfolio;
 import com.wbm.scenergyspring.domain.portfolio.repository.PortfolioRepository;
 import com.wbm.scenergyspring.domain.portfolio.service.command.CreatePortfolioCommand;
 import com.wbm.scenergyspring.domain.portfolio.service.command.UpdatePortfolioCommand;
+import com.wbm.scenergyspring.global.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +26,15 @@ public class PortfolioService {
     }
     @Transactional(readOnly = false)
     public Long updatePortfolio(UpdatePortfolioCommand command) {
-        Portfolio portfolio = portfolioRepository.findById(command.getPortfolioId()).get();
-        portfolio.updatePortfolio(portfolio, command);
+        Portfolio portfolio = portfolioRepository.findById(command.getPortfolioId())
+                .orElseThrow(()-> new EntityNotFoundException("DB에 저장되지 않은 포트폴리오"));
+        portfolio.updatePortfolio(
+                command.getDescription(),
+                command.getExperiences(),
+                command.getHonors(),
+                command.getEtcs(),
+                command.getEducations()
+        );
         return command.getPortfolioId();
     }
 }
