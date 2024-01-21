ba6ecbae03a831764ae1a46ec9964a6127c1a8a5
bluepeng9 <bluepeng9@gmail.com>
2024-01-21 20:30:43 +0900

    feat: 엔티티 존재 예외 추가

---
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/global/exception/EntityAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.wbm.scenergyspring.global.exception;
+
+public class EntityAlreadyExistException extends BusinessException {
+    public EntityAlreadyExistException(String message) {
+        super(message);
+    }
+}
