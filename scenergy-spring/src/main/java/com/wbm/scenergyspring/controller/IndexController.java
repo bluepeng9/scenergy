6813206f0251bd0165538127eaf3085e61694a22
haeji <hjlove001@gmail.com>
2024-01-21 22:29:06 +0900

    fix: IndexController 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/controller/IndexController.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/controller/IndexController.java
@@ -1,8 +1,10 @@
 package com.wbm.scenergyspring.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-public class IntexController {
+@Controller
+public class IndexController {
 	@GetMapping("/login")
 	public  String loginForm() {
 		return "loginForm";
