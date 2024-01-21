ad5acf1b8ab73a980b6c81b84490ef298efeb506
haeji <hjlove001@gmail.com>
2024-01-21 22:01:37 +0900

    feat: SecurityConfig 추가

---
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.wbm.scenergyspring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeRequests()
+			.requestMatchers( "/user/**").authenticated()
+			.requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/loginForm")
+			.loginProcessingUrl("login")
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm");
+
+		return http.build();
+	}
+}
