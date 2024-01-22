31f54939201721bcca7ef10aee3bc264fc3b9b45
imin <lllgee@naver.com>
2024-01-22 13:58:58 +0900

    feat: GenreTag class getter 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/entity/GenreTag.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/entity/GenreTag.java
@@ -1,12 +1,14 @@
 package com.wbm.scenergyspring.domain.tag.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 
 /**
  * 0:전체, 1:팝, 2:발라드, 3:인디, 4:,힙합, 5:락, 6:R&B, 7:재즈, 8:클래식, 9: 기타
  */
 @Entity
+@Getter
 public class GenreTag {
 
     @Id
