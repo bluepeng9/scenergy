f9fa8e13aa7d2b13c174025e9a210d68665ab57a
imin <lllgee@naver.com>
2024-01-22 13:59:08 +0900

    feat: InstrumentTag class getter 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/entity/InstrumentTag.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/entity/InstrumentTag.java
@@ -1,11 +1,13 @@
 package com.wbm.scenergyspring.domain.tag.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 /**
  * 0:전체, 1:기타, 2:드럼, 3:베이스, 4:키보드, 5:보컬, 6:클래식, 7:기타
  */
 @Entity
+@Getter
 public class InstrumentTag {
 
     @Id
