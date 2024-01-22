f19c9399bc3c987244cec9004313b49b8a704af7
haeji <hjlove001@gmail.com>
2024-01-22 10:53:18 +0900

    fix: PrincipalDetails 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/config/auth/PrincipalDetailService.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/config/auth/PrincipalDetailService.java
@@ -1,33 +1,33 @@
-package com.wbm.scenergyspring.config.auth;
-
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import com.wbm.scenergyspring.domain.user.entity.User;
-
-import com.wbm.scenergyspring.domain.user.repository.UserRepository;
-
-/**
- * 로그인 요청 오면 유저 이름이 있는지 확인
- */
-
-@Service
-public class PrincipalDetailService implements UserDetailsService {
-
-	@Autowired
-	UserRepository userRepository;
-
-	// 이것두 username으로 찾아야하는데 repository에 findByUsername 추가해도 되는지??????
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Optional<User> userEntity = userRepository.findById(Long.parseLong(username));
-		if (userEntity.isPresent()) {
-			return new PrincipalDetails(userEntity);
-		}
-		return null;
-	}
-}
+// package com.wbm.scenergyspring.config.auth;
+//
+// import java.util.Optional;
+//
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+// import org.springframework.stereotype.Service;
+// import com.wbm.scenergyspring.domain.user.entity.User;
+//
+// import com.wbm.scenergyspring.domain.user.repository.UserRepository;
+//
+// /**
+//  * 로그인 요청 오면 유저 이름이 있는지 확인
+//  */
+//
+// @Service
+// public class PrincipalDetailService implements UserDetailsService {
+//
+// 	@Autowired
+// 	UserRepository userRepository;
+//
+// 	// 이것두 username으로 찾아야하는데 repository에 findByUsername 추가해도 되는지??????
+// 	@Override
+// 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+// 		Optional<User> userEntity = userRepository.findById(Long.parseLong(username));
+// 		if (userEntity.isPresent()) {
+// 			return new PrincipalDetails(userEntity);
+// 		}
+// 		return null;
+// 	}
+// }
--- scenergy-spring/src/main/java/com/wbm/scenergyspring/config/auth/PrincipalDetails.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/config/auth/PrincipalDetails.java
@@ -1,6 +1,5 @@
 package com.wbm.scenergyspring.config.auth;
 
-import java.security.Key;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
