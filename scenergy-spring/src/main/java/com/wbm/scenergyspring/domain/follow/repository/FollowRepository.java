d139e96bf950653263c36ff8e0cfa23af19947c9
bluepeng9 <bluepeng9@gmail.com>
2024-01-23 15:42:08 +0900

    feat: followUser 이미 팔로잉 하는 예외 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/follow/repository/FollowRepository.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/follow/repository/FollowRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.wbm.scenergyspring.domain.follow.entity.Follow;
+import com.wbm.scenergyspring.domain.user.entity.User;
 
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+	boolean existsByFromAndTo(User from, User to);
 }
--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/follow/service/FollowService.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/follow/service/FollowService.java
@@ -8,6 +8,7 @@ import com.wbm.scenergyspring.domain.follow.repository.FollowRepository;
 import com.wbm.scenergyspring.domain.follow.service.command.FollowUserCommand;
 import com.wbm.scenergyspring.domain.user.entity.User;
 import com.wbm.scenergyspring.domain.user.repository.UserRepository;
+import com.wbm.scenergyspring.global.exception.EntityAlreadyExistException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,15 +20,28 @@ public class FollowService {
 	final FollowRepository followRepository;
 	final UserRepository userRepository;
 
+	/**
+	 *
+	 * @param command
+	 * @return
+	 * @throws com.wbm.scenergyspring.global.exception.EntityAlreadyExistException 이미 팔로우 하는 경우
+	 */
 	@Transactional
 	public Long followUser(FollowUserCommand command) {
 		User fromUser = userRepository.getReferenceById(command.getFromUserId());
 		User toUser = userRepository.getReferenceById(command.getToUserId());
 
+		boolean alreadyFollow = followRepository.existsByFromAndTo(fromUser, toUser);
+
+		if (alreadyFollow) {
+			throw new EntityAlreadyExistException("이미 팔로우 중인 사용자 입니다.");
+		}
+
 		Follow follow = Follow.createFollow(
 			fromUser,
 			toUser
 		);
+
 		Follow save = followRepository.save(follow);
 		return save.getId();
 	}
