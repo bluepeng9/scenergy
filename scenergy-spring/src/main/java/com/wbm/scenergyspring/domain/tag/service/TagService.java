295fb2524e24dd0cc938baf8f5640fb7ea072102
imin <lllgee@naver.com>
2024-01-25 02:24:08 +0900

    feat: TagService location 관련 기능 삭제 및 장르 & 악기 RUD 기능 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/service/TagService.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/service/TagService.java
@@ -2,13 +2,16 @@ package com.wbm.scenergyspring.domain.tag.service;
 
 import com.wbm.scenergyspring.domain.tag.entity.GenreTag;
 import com.wbm.scenergyspring.domain.tag.entity.InstrumentTag;
-import com.wbm.scenergyspring.domain.tag.entity.LocationTag;
 import com.wbm.scenergyspring.domain.tag.repository.GenreTagRepository;
 import com.wbm.scenergyspring.domain.tag.repository.InstrumentTagRepository;
-import com.wbm.scenergyspring.domain.tag.repository.LocationTagRepository;
+import com.wbm.scenergyspring.domain.tag.service.command.GenreTagCommand;
+import com.wbm.scenergyspring.domain.tag.service.command.InstrumentTagCommand;
+import com.wbm.scenergyspring.global.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,7 +20,6 @@ public class TagService {
 
     private final GenreTagRepository genreTagRepository;
     private final InstrumentTagRepository instrumentTagRepository;
-    private final LocationTagRepository locationTagRepository;
 
     @Transactional(readOnly = false)
     public boolean createGenreTag(String genreName){
@@ -27,20 +29,49 @@ public class TagService {
         return true;
     }
 
+    public List<GenreTag> getAllGenreTags() {
+        return genreTagRepository.findAll();
+    }
+
     @Transactional(readOnly = false)
-    public boolean createInstrumentName(String instrumentName){
+    public String updateGenreTag(GenreTagCommand command) {
+        GenreTag genreTag = genreTagRepository.findByGenreName(command.getGenreTagName()).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 장르입니다."));
+        genreTag.updateGenreTag(command.getChangeGenreTagName());
+        return genreTag.getGenreName();
+    }
+
+    @Transactional(readOnly = false)
+    public boolean deleteGenreTag(String genreName) {
+        GenreTag genreTag = genreTagRepository.findByGenreName(genreName).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 장르입니다."));
+        genreTagRepository.delete(genreTag);
+        return true;
+    }
+
+    @Transactional(readOnly = false)
+    public boolean createInstrumentTag(String instrumentName) {
         if(instrumentTagRepository.existsByInstrumentName(instrumentName))
             return false;
         instrumentTagRepository.save(InstrumentTag.createInstrumentTag(instrumentName));
         return true;
     }
 
+    public List<InstrumentTag> getAllInstrumentTags() {
+        return instrumentTagRepository.findAll();
+    }
+
     @Transactional(readOnly = false)
-    public boolean createLocationTag(String locationName){
-        if(locationTagRepository.existsByLocationName(locationName))
-            return false;
-        locationTagRepository.save(LocationTag.createLocationTag(locationName));
+    public String updateInstrumentTag(InstrumentTagCommand command) {
+        InstrumentTag instrumentTag = instrumentTagRepository.findByInstrumentName(command.getInstrumentTagName()).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 악기입니다."));
+        instrumentTag.updateInstrumentTag(command.getChangeInstrumentTagName());
+        return instrumentTag.getInstrumentName();
+    }
+
+    @Transactional(readOnly = false)
+    public boolean deleteInstrumentTag(String instrumentName) {
+        InstrumentTag instrumentTag = instrumentTagRepository.findByInstrumentName(instrumentName).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 악기입니다."));
+        instrumentTagRepository.delete(instrumentTag);
         return true;
     }
+
 
 }
