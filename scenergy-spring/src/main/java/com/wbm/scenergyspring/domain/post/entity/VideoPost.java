952343eb8b837e1b9ebc40c5ec652409d7ab1ee8
imin <lllgee@naver.com>
2024-01-25 02:30:28 +0900

    feat: VideoPost videoPostInstrumentTags변수 추가 및 CUD 기능을 위한 DTO 생성 함수 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/entity/VideoPost.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/entity/VideoPost.java
@@ -1,6 +1,8 @@
 package com.wbm.scenergyspring.domain.post.entity;
 
-import com.wbm.scenergyspring.domain.tag.entity.VideoPostGenreTag;
+import com.wbm.scenergyspring.domain.post.service.command.UpdateVideoPostCommand;
+import com.wbm.scenergyspring.domain.post.service.command.VideoPostGenreTagCommand;
+import com.wbm.scenergyspring.domain.post.service.command.VideoPostInstrumentTagCommand;
 import com.wbm.scenergyspring.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -27,6 +29,9 @@ public class VideoPost extends Post {
     @OneToMany(mappedBy = "videoPost", cascade = CascadeType.ALL)
     private List<VideoPostGenreTag> videoPostGenreTags = new ArrayList<>();
 
+    @OneToMany(mappedBy = "videoPost", cascade = CascadeType.ALL)
+    private List<VideoPostInstrumentTag> videoPostInstrumentTags = new ArrayList<>();
+
     public static VideoPost createVideoPost(
         User user,
         Video video,
@@ -42,5 +47,44 @@ public class VideoPost extends Post {
         videopost.writer = writer;
 
         return videopost;
+    }
+
+    public void updateVideoPost(UpdateVideoPostCommand command) {
+        if (command.getPostTitle() != null)
+            title = command.getPostTitle();
+        if (command.getPostContent() != null)
+            content = command.getPostContent();
+    }
+
+    public List<VideoPostGenreTagCommand> createVideoPostGenreTagCommand() {
+        List<VideoPostGenreTagCommand> list = new ArrayList<>();
+        for (VideoPostGenreTag videoPostGenreTag : videoPostGenreTags) {
+            VideoPostGenreTagCommand videoPostGenreTagCommand = VideoPostGenreTagCommand.builder()
+                    .id(videoPostGenreTag.getId())
+                    .genreTag(videoPostGenreTag.getGenreTag())
+                    .videoPost(videoPostGenreTag.getVideoPost())
+                    .build();
+        }
+        return list;
+    }
+
+    public List<VideoPostInstrumentTagCommand> createVideoPostInstrumentTagCommand() {
+        List<VideoPostInstrumentTagCommand> list = new ArrayList<>();
+        for (VideoPostInstrumentTag videoPostInstrumentTag : videoPostInstrumentTags) {
+            VideoPostInstrumentTagCommand videoPostInstrumentTagCommand = VideoPostInstrumentTagCommand.builder()
+                    .id(videoPostInstrumentTag.getId())
+                    .instrumentTag(videoPostInstrumentTag.getInstrumentTag())
+                    .videoPost(videoPostInstrumentTag.getVideoPost())
+                    .build();
+        }
+        return list;
+    }
+
+    public void deleteVideoPostGenreTags() {
+        videoPostGenreTags = null;
+    }
+
+    public void deleteVideoPostInstrumentTags() {
+        videoPostInstrumentTags = null;
     }
 }
