c7670d50b2a1db6a755638f577e6be5b8ab4dc27
bluepeng9 <bluepeng9@gmail.com>
2024-01-24 09:58:39 +0900

    feat: LikeRepository 추가

---
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/like/repository/LikeRepository.java
@@ -0,0 +1,16 @@
+package com.wbm.scenergyspring.domain.like.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wbm.scenergyspring.domain.like.Like;
+import com.wbm.scenergyspring.domain.post.entity.VideoPost;
+import com.wbm.scenergyspring.domain.user.entity.User;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+	Optional<Object> findByVideoPostAndUser(VideoPost videoPost, User user);
+}
