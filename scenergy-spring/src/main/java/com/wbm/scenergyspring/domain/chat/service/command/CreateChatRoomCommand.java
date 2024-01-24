9e5e7fdb9e3264c55f5b62650fa81d470427904e
pedada <korea9300@naver.com>
2024-01-24 15:19:07 +0900

    feat: ChatService 관련 DTO 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/chat/service/command/CreateChatRoomCommand.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/chat/service/command/CreateChatRoomCommand.java
@@ -1,11 +1,13 @@
 package com.wbm.scenergyspring.domain.chat.service.command;
 
 import com.wbm.scenergyspring.domain.chat.entity.ChatUser;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class CreateChatRoomCommand {
     private String name;
     private int status;
