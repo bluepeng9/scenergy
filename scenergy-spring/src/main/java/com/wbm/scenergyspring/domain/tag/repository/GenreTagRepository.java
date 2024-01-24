70e8460109576cdff2a2c5321e48ae95db4bddd0
imin <lllgee@naver.com>
2024-01-25 02:19:21 +0900

    refactor: GenreTagRepository findByGenreName() 메서드 반환 타입 변경

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/repository/GenreTagRepository.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/repository/GenreTagRepository.java
@@ -4,11 +4,13 @@ import com.wbm.scenergyspring.domain.tag.entity.GenreTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GenreTagRepository extends JpaRepository<GenreTag, Long> {
 
     Boolean existsByGenreName (String genreName);
 
-    GenreTag findByGenreName(String genreName);
+    Optional<GenreTag> findByGenreName(String genreName);
 
 }
