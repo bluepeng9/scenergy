c735273841fa578703f9ace1962aa67b3f9714b4
imin <lllgee@naver.com>
2024-01-25 02:22:05 +0900

    feat: InstrumentTagRepository findByInstrumentName() 메서드 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/repository/InstrumentTagRepository.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/repository/InstrumentTagRepository.java
@@ -4,9 +4,13 @@ import com.wbm.scenergyspring.domain.tag.entity.InstrumentTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InstrumentTagRepository extends JpaRepository<InstrumentTag,Long> {
 
-    Boolean existsByInstrumentName (String InstrumentName);
+    Boolean existsByInstrumentName(String instrumentName);
+
+    Optional<InstrumentTag> findByInstrumentName(String genreName);
 
 }
