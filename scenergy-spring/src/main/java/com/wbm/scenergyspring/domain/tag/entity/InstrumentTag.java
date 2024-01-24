5d1eb305b582c4c99c71614b3f64838e896e0c3b
imin <lllgee@naver.com>
2024-01-25 02:21:28 +0900

    feat: InstrumentTag UpdateInstrumentTag() 메서드 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/entity/InstrumentTag.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/entity/InstrumentTag.java
@@ -1,10 +1,13 @@
 package com.wbm.scenergyspring.domain.tag.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 
 /**
- * 0:전체, 1:기타, 2:드럼, 3:베이스, 4:키보드, 5:보컬, 6:클래식, 7:기타
+ * 1:전체, 2:기타, 3:드럼, 4:베이스, 5:키보드, 6:보컬, 7:클래식, 8:기타
  */
 @Entity
 @Getter
@@ -14,11 +17,15 @@ public class InstrumentTag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    String InstrumentName;
+    String instrumentName;
+
+    public void updateInstrumentTag(String instrumentName) {
+        this.instrumentName = instrumentName;
+    }
 
     public static InstrumentTag createInstrumentTag(String InstrumentName){
         InstrumentTag instrumentTag = new InstrumentTag();
-        instrumentTag.InstrumentName = InstrumentName;
+        instrumentTag.instrumentName = InstrumentName;
         return instrumentTag;
     }
 
