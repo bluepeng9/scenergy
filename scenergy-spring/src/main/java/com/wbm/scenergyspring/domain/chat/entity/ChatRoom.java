50e1f8755bdfd5093b30ed673c07a005897135c5
pedada <korea9300@naver.com>
2024-01-25 11:06:41 +0900

    feat: ChatRoom.inviteChatUsers, rename 추가
    채팅맴버 추가, 채팅방 이름 변경 메서드

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/chat/entity/ChatRoom.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/chat/entity/ChatRoom.java
@@ -1,11 +1,13 @@
 package com.wbm.scenergyspring.domain.chat.entity;
 
+import com.wbm.scenergyspring.domain.user.entity.User;
 import com.wbm.scenergyspring.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -32,13 +34,30 @@ public class ChatRoom extends BaseEntity {
 
     public static ChatRoom createNewRoom(
             String name,
-            int status,
-            List<ChatUser> chatUsers
+            int status
     ) {
         ChatRoom chatRoom = new ChatRoom();
         chatRoom.name = name;
         chatRoom.status = status;
-        chatRoom.chatUsers.addAll(chatUsers);
+        chatRoom.chatUsers = new ArrayList<>();
+        chatRoom.chatMessages = new ArrayList<>();
+        ChatMessage firstMessage = ChatMessage.createChatMessage(0L, "채팅방이 시작되었습니다.", chatRoom);
+        chatRoom.chatMessages.add(firstMessage);
         return chatRoom;
+    }
+
+    public void addChatMessages(ChatMessage chatMessage) {
+        chatMessages.add(chatMessage);
+    }
+
+    public void inviteChatUsers(List<User> users) {
+        for (User user : users) {
+            ChatUser chatUser = ChatUser.createChatUser(this, user);
+            chatUsers.add(chatUser);
+        }
+    }
+
+    public void rename(String roomName) {
+        this.name = roomName;
     }
 }
