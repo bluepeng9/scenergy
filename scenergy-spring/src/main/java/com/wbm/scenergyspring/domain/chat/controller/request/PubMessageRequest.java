8a4adacfda46b98a4e990299a41c4752ba213156
pedada <korea9300@naver.com>
2024-01-25 11:09:33 +0900

    feat: CreatePubMessageCommand수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/chat/controller/request/PubMessageRequest.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/chat/controller/request/PubMessageRequest.java
@@ -1,21 +1,23 @@
 package com.wbm.scenergyspring.domain.chat.controller.request;
 
 import com.wbm.scenergyspring.domain.chat.service.command.CreatePubMessageCommand;
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class PubMessageRequest {
     Long user_id;
     Long room_id;
     String message;
+    String messageType;
 
     public CreatePubMessageCommand toCreatePubMessageCommand() {
         CreatePubMessageCommand command = CreatePubMessageCommand.builder()
                 .userId(user_id)
                 .roomId(room_id)
                 .messageText(message)
+                .messageType(messageType)
                 .build();
         return command;
     }
