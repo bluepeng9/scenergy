5e4dfde0879146392da872563095491d8649de5d
bluepeng9 <bluepeng9@gmail.com>
2024-01-25 14:15:37 +0900

    test: UserGenerator 추가

---
+++ scenergy-spring/src/test/java/com/wbm/scenergyspring/util/UserGenerator.java
@@ -0,0 +1,29 @@
+package com.wbm.scenergyspring.util;
+
+import java.util.Random;
+
+import com.wbm.scenergyspring.domain.user.entity.User;
+
+public class UserGenerator {
+	private static int userCount = 1;
+
+	public static User createRandomMember() {
+		String email = "asdf" + userCount + "@test.com";
+		User user = User.createNewUser(
+			email,
+			"password" + userCount,
+			"M",
+			getRandomGender()
+		);
+		userCount += 1;
+		return user;
+	}
+
+	private static String getRandomGender() {
+		Random random = new Random();
+
+		String s = random.nextInt() > 0 ? "M" : "F";
+
+		return UserGenerator.getRandomGender();
+	}
+}
