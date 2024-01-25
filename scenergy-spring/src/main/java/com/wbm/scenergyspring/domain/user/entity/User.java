82d2482c753c38fa360ea0a75b53f6b3aa140638
pedada <korea9300@naver.com>
2024-01-25 10:29:00 +0900

    feat: User Entity name 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/user/entity/User.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/user/entity/User.java
@@ -1,15 +1,7 @@
 package com.wbm.scenergyspring.domain.user.entity;
 
 import com.wbm.scenergyspring.global.entity.BaseEntity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
