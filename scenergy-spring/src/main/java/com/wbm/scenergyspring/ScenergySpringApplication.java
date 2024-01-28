8f13d6f7b52b4510c23e807d0ef00e42b39b40dc
imin <lllgee@naver.com>
2024-01-28 17:44:16 +0900

    remove: PathProperties 삭제

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/ScenergySpringApplication.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/ScenergySpringApplication.java
@@ -1,10 +1,8 @@
 package com.wbm.scenergyspring;
 
-import com.wbm.scenergyspring.global.config.PathProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
