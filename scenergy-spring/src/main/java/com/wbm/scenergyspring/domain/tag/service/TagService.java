0c2bc181ec96b007ba5a1d0f6011cc7860acfbfa
imin <lllgee@naver.com>
2024-01-29 17:22:24 +0900

    feat: VideoPostService 예외처리 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/service/TagService.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/tag/service/TagService.java
@@ -6,6 +6,7 @@ import com.wbm.scenergyspring.domain.tag.repository.GenreTagRepository;
 import com.wbm.scenergyspring.domain.tag.repository.InstrumentTagRepository;
 import com.wbm.scenergyspring.domain.tag.service.command.GenreTagCommand;
 import com.wbm.scenergyspring.domain.tag.service.command.InstrumentTagCommand;
+import com.wbm.scenergyspring.global.exception.BusinessException;
 import com.wbm.scenergyspring.global.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,12 +25,11 @@ public class TagService {
 
     @Transactional(readOnly = false)
     public String createGenreTag(String genreName) {
-
         if (!StringUtils.hasText(genreName))
-            return "장르가 입력되지 않았습니다.";
+            throw new BusinessException("장르가 입력되지 않았습니다.");
         String str = StringUtils.trimWhitespace(genreName);
         if (genreTagRepository.existsByGenreName(str))
-            return "이미 입력된 장르입니다.";
+            throw new BusinessException("이미 입력된 장르입니다.");
         genreTagRepository.save(GenreTag.createGenreTag(str));
         return genreName;
     }
@@ -48,7 +48,7 @@ public class TagService {
     @Transactional(readOnly = false)
     public String deleteGenreTag(String genreName) {
         if (!StringUtils.hasText(genreName))
-            return "장르가 입력되지 않았습니다.";
+            throw new BusinessException("장르가 입력되지 않았습니다.");
         String str = StringUtils.trimWhitespace(genreName);
         GenreTag genreTag = genreTagRepository.findByGenreName(str).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 장르입니다."));
         genreTagRepository.delete(genreTag);
@@ -58,10 +58,10 @@ public class TagService {
     @Transactional(readOnly = false)
     public String createInstrumentTag(String instrumentName) {
         if (!StringUtils.hasText(instrumentName))
-            return "악기가 입력되지 않았습니다.";
+            throw new BusinessException("악기가 입력되지 않았습니다.");
         String str = StringUtils.trimWhitespace(instrumentName);
         if (instrumentTagRepository.existsByInstrumentName(str))
-            return "이미 입력된 악기입니다.";
+            throw new BusinessException("이미 입력된 장르입니다.");
         instrumentTagRepository.save(InstrumentTag.createInstrumentTag(str));
         return instrumentName;
     }
@@ -80,7 +80,7 @@ public class TagService {
     @Transactional(readOnly = false)
     public String deleteInstrumentTag(String instrumentName) {
         if (!StringUtils.hasText(instrumentName))
-            return "악기가 입력되지 않았습니다.";
+            throw new BusinessException("악기가 입력되지 않았습니다.");
         InstrumentTag instrumentTag = instrumentTagRepository.findByInstrumentName(instrumentName).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 악기입니다."));
         instrumentTagRepository.delete(instrumentTag);
         return instrumentName;
