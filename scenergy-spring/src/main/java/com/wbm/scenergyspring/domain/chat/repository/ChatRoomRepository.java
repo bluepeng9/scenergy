6e027783b9bcdb5765031f9cc879833cc392baea
pedada <korea9300@naver.com>
2024-01-29 22:47:10 +0900

    feat: ChatRoomRepository findMyChatRoom method 추가

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/chat/repository/ChatRoomRepository.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/chat/repository/ChatRoomRepository.java
@@ -2,8 +2,19 @@ package com.wbm.scenergyspring.domain.chat.repository;
 
 import com.wbm.scenergyspring.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    @Query("""
+            select distinct cr from ChatRoom cr
+            join fetch cr.chatUsers cu
+            join fetch cu.user u
+            where u.id = :userId           
+            """)
+    List<ChatRoom> findMyChatRoom(@Param("userId") Long userId);
 }
