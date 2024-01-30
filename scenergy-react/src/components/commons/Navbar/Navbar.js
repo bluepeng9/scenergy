da784b11e95872b79d1bbda3009c3eb20af555c6
김은지 <eungg0214@naver.com>
2024-01-30 22:27:37 +0900

    feat: navBar ui 추가

--- scenergy-react/src/components/commons/Navbar/Navbar.js
+++ scenergy-react/src/components/commons/Navbar/Navbar.js
@@ -1,40 +1,53 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import {useState} from "react";
 
 const Navbar = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  return (
-    <div className={styles.NavBarContainer}>
-      <div className={styles.ScenergyTab}>
-        <Link to="/">scenergy</Link>
-      </div>
-      <Link to="/home">홈</Link>
-      <Link to="/search">검색</Link>
-      <div className={styles.NavBarDropdownGlobal}>
-        <Link to="/scenergy" className={styles.NavScenergy}>
-          시너지
-        </Link>
-        <div className={styles.NavBarDropMenu}>
-          <Link to="/scenergy">시너지</Link>
-          <Link to="/scenergy/mypost">
-            <div>내가 쓴 글</div>
-          </Link>
-          <Link to="/scenergy/myapply">
-            <div>내가 지원한 글</div>
-          </Link>
-          <Link to="/scenergy/bookmark">
-            <div>북마크</div>
-          </Link>
-        </div>
-      </div>
-      <Link to="/videoupload">업로드</Link>
-      <div className={styles.NavBarNotice}>
-        <p>알림</p>
-      </div>
-
-      <Link to="/chat">메세지</Link>
-      <Link to="/profile">프로필</Link>
-    </div>
-  );
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+    return (
+        <nav className={styles.NavBarContainer}>
+            <ul>
+                <li className={styles.ScenergyTab}>
+                    <Link to="/">scenergy</Link>
+                </li>
+                <li>
+                    <Link to="/home">홈</Link>
+                </li>
+                <li>
+                    <Link to="/search">검색</Link>
+                </li>
+                <li
+                    className={`${styles.NavBarDropdownGlobal} ${dropdownOpen ? styles.dropdownOpen : ""}`}
+                    onMouseEnter={toggleDropdown}
+                    onMouseLeave={toggleDropdown}
+                >
+                    <Link to="/scenergy" className={styles.NavScenergy}>시너지</Link>
+                    <ul className={styles.NavBarDropMenu}>
+                        <li><Link to="/scenergy">시너지</Link></li>
+                        <li><Link to="/scenergy/mypost">내가 쓴 글</Link></li>
+                        <li><Link to="/scenergy/myapply">내가 지원한 글</Link></li>
+                        <li><Link to="/scenergy/bookmark">북마크</Link></li>
+                    </ul>
+                </li>
+                <li>
+                    <Link to="/videoupload">업로드</Link>
+                </li>
+                <li className={styles.NavBarNotice}>
+                    <p>알림</p>
+                </li>
+                <li>
+                    <Link to="/chat">메세지</Link>
+                </li>
+                <li>
+                    <Link to="/profile">프로필</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
+
 export default Navbar;
