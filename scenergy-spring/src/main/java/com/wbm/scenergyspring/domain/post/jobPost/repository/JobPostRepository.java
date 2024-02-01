4529b5d3715e67ecf0e220f98ee08ee222ee65e3
haeji <hjlove001@gmail.com>
2024-02-01 20:56:18 +0900

    fix: JobPostRepository 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/jobPost/repository/JobPostRepository.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/jobPost/repository/JobPostRepository.java
@@ -1,8 +1,10 @@
 package com.wbm.scenergyspring.domain.post.jobPost.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPost;
 
+@Repository
 public interface JobPostRepository extends JpaRepository<JobPost, Long> {
 }
