77c1373adcb96397feaa3072e398bce26bcb9599
haeji <hjlove001@gmail.com>
2024-02-01 20:58:37 +0900

    fix: UpdateJobPostCommand 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/jobPost/controller/request/UpdateJobPostRequest.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/jobPost/controller/request/UpdateJobPostRequest.java
@@ -1,33 +1,56 @@
 package com.wbm.scenergyspring.domain.post.jobPost.controller.request;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.wbm.scenergyspring.domain.post.jobPost.entity.IsActive;
-import com.wbm.scenergyspring.domain.post.jobPost.service.Command.UpdateJobPostcommand;
 
+import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPost;
+import com.wbm.scenergyspring.domain.post.jobPost.service.Command.UpdateJobPostCommand;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UpdateJobPostRequest {
 
-	Long jobPostId;
 	String title;
 	String content;
 	LocalDateTime expirationDate;
 	Long peopleRecruited;
 	Long bookMark;
 	IsActive isActive;
+	private List<Long> instrumentTags;
+	private List<Long> locationTags;
+	private List<Long> genreTags;
 
-	public UpdateJobPostcommand toUpdateJobPost() {
-		UpdateJobPostcommand command = UpdateJobPostcommand.builder()
-			.jobPostId(getJobPostId())
-			.title(getTitle())
-			.content(getContent())
-			.expirationDate(getExpirationDate())
-			.peopleRecruited(getPeopleRecruited())
-			.bookMark(getBookMark())
-			.isActive(getIsActive())
+	public UpdateJobPostCommand toCreateJobPost(JobPost jobPost) {
+		UpdateJobPostCommand command = UpdateJobPostCommand.builder()
+			.title(title)
+			.content(content)
+			.expirationDate(expirationDate)
+			.peopleRecruited(peopleRecruited)
+			.bookMark(bookMark)
+			.isActive(isActive)
+			// .instrumentTagIds(instrumentTags)
+			// .locationTagIds(locationTags)
+			// .genreTagIds(genreTags)
 			.build();
 		return command;
 	}
+
+	public UpdateJobPostCommand updateJobPostCommand() {
+		UpdateJobPostCommand command = UpdateJobPostCommand.builder()
+			.title(title)
+			.content(content)
+			.expirationDate(expirationDate)
+			.peopleRecruited(peopleRecruited)
+			.bookMark(bookMark)
+			.isActive(isActive)
+			// .instrumentTagIds()
+			.build();
+		return command;
+
+	}
 }
+
