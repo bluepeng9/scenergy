d38c3e4072c115534adb91e21806ca17d6ed3b58
bluepeng9 <bluepeng9@gmail.com>
2024-02-01 21:54:34 +0900

    fix: CreateUserRequest 충돌 후 에러 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/user/controller/request/CreateUserRequest.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/user/controller/request/CreateUserRequest.java
@@ -7,17 +7,19 @@ import lombok.Data;
 
 @Data
 public class CreateUserRequest {
-    String email;
-    String password;
+	String email;
+	String password;
 	Gender gender;
 	String username;
+	String nickname;
 
-    public CreateUserCommand toCreateUserCommand() {
-        CreateUserCommand command = new CreateUserCommand();
-		command.setEmail(getEmail());
-		command.setPassword(getPassword());
-		command.setGender(getGender());
-		command.setUsername(getUsername());
-        return command;
-    }
+	public CreateUserCommand toCreateUserCommand() {
+		return CreateUserCommand.builder()
+			.email(email)
+			.password(password)
+			.gender(gender)
+			.username(username)
+			.nickname(nickname)
+			.build();
+	}
 }
