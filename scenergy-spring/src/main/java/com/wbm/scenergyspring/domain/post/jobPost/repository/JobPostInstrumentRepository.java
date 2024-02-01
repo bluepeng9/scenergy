6b5d9e23c44f8595eb71972b998d131072b6c79e
haeji <hjlove001@gmail.com>
2024-02-01 21:03:34 +0900

    feat: JobPostInstrumentRepository 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/jobPost/repository/JobPostInstrumentRepository.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/jobPost/repository/JobPostInstrumentRepository.java
@@ -1,8 +1,10 @@
 package com.wbm.scenergyspring.domain.post.jobPost.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPostInstrumentTag;
 
-public interface JobPostInstrumentrepository extends JpaRepository<JobPostInstrumentTag, Long> {
+@Repository
+public interface JobPostInstrumentRepository extends JpaRepository<JobPostInstrumentTag, Long> {
 }
