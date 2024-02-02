a72f553cd3a2f21dfe0d36ea349c708fe001314e
bluepeng9 <bluepeng9@gmail.com>
2024-02-02 16:33:21 +0900

    feat: notification 모듈 생성

---
+++ scenergy-notification/src/main/java/com/example/scenergynotification/ScenergyNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.scenergynotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenergyNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenergyNotificationApplication.class, args);
+	}
+
+}
---
+++ scenergy-notification/src/test/java/com/example/scenergynotification/ScenergyNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scenergynotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenergyNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
