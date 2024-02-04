7cec23e842725de082461b3b34e5c5a0d8b5c306
haeji <hjlove001@gmail.com>
2024-02-04 19:50:51 +0900

    feat: JobPostApplyRepository 추가

---
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/jobPost/repository/JobPostApplyRepository.java
@@ -0,0 +1,14 @@
+package com.wbm.scenergyspring.domain.post.jobPost.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPost;
+import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPostApply;
+import com.wbm.scenergyspring.domain.user.entity.User;
+
+@Repository
+public interface JobPostApplyRepository extends JpaRepository<JobPostApply, Long> {
+	JobPostApply findByJobPostAndUser(JobPost jobPost, User user);
+	JobPostApply findApplyByJobPost(JobPost jobPost);
+}
