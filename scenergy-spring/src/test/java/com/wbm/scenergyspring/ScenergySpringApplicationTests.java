56e0614d48e004906e6be4cc077e2c3a7b4c0445
bluepeng9 <bluepeng9@gmail.com>
2024-02-05 11:09:50 +0900

    test: gradle build 에러 수정

--- scenergy-spring/src/test/java/com/wbm/scenergyspring/ScenergySpringApplicationTests.java
+++ scenergy-spring/src/test/java/com/wbm/scenergyspring/ScenergySpringApplicationTests.java
@@ -3,7 +3,7 @@ package com.wbm.scenergyspring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ScenergySpringApplication.class)
 class ScenergySpringApplicationTests {
 
     @Test
