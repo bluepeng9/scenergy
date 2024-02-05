cc516d183d04c2b7d3ee5584ba6b23ee21afb831
imin <lllgee@naver.com>
2024-02-05 17:13:23 +0900

    feat: VideoPostRepository findAllByFollowing param 수정

--- scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/videoPost/repository/VideoPostRepository.java
+++ scenergy-spring/src/main/java/com/wbm/scenergyspring/domain/post/videoPost/repository/VideoPostRepository.java
@@ -3,6 +3,7 @@ package com.wbm.scenergyspring.domain.post.videoPost.repository;
 import com.wbm.scenergyspring.domain.post.videoPost.entity.VideoPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,5 @@ public interface VideoPostRepository extends JpaRepository<VideoPost, Long> {
 
     @Query("select vp from VideoPost vp " +
             "where vp.user.id in (select f.to.id from Follow f where f.from.id=:id)")
-    List<VideoPost> findAllByFollowing(Long id);
+    List<VideoPost> findAllByFollowing(@Param("id") Long id);
 }
