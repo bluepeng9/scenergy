b014261e6f0dcc48fb2a21727cd7d1df219a96e8
haeji <hjlove001@gmail.com>
2024-02-07 21:44:20 +0900

    fix: api 수정

--- scenergy-react/src/apis/ScenergyApi.js
+++ scenergy-react/src/apis/ScenergyApi.js
@@ -1,16 +1,9 @@
-import axios from "axios";
 import api from "./api";
 
-export const scenergy = {
-  getAll: (data) => {
-    return {
-      IsActive: "active",
-      nickname: "허허",
-      title: "허허허허",
-      content: "드럼 1명 구합니다",
-      expirationDate: "2024-03-01",
-      totalCount: "34",
-    };
-    return api.get("jobPost/getAll");
-  },
+const getAllJobPost = async (data) => {
+  console.log(data);
+  const result = await api.get("jobPost/getAll");
+  console.log(result);
+  return result.data;
 };
+export { getAllJobPost };
