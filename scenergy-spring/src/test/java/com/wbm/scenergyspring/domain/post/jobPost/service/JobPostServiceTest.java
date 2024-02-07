fe435e2ae3b2b9f3239335b535fb4f91015cb8e0
imin <lllgee@naver.com>
2024-02-08 00:03:59 +0900

    test: JopPost 검색 기능 테스트

--- scenergy-spring/src/test/java/com/wbm/scenergyspring/domain/post/jobPost/service/JobPostServiceTest.java
+++ scenergy-spring/src/test/java/com/wbm/scenergyspring/domain/post/jobPost/service/JobPostServiceTest.java
@@ -1,34 +1,14 @@
 package com.wbm.scenergyspring.domain.post.jobPost.service;
 
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.wbm.scenergyspring.domain.post.jobPost.controller.request.UpdateJobPostRequest;
 import com.wbm.scenergyspring.domain.post.jobPost.controller.response.GetJobPostCommandResponse;
-import com.wbm.scenergyspring.domain.post.jobPost.entity.IsActive;
-import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPost;
-import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPostGenreTag;
-import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPostInstrumentTag;
-import com.wbm.scenergyspring.domain.post.jobPost.entity.JobPostLocationTag;
+import com.wbm.scenergyspring.domain.post.jobPost.controller.response.SearchAllJobPostResponse;
+import com.wbm.scenergyspring.domain.post.jobPost.entity.*;
 import com.wbm.scenergyspring.domain.post.jobPost.repository.JobBookMarkRepository;
 import com.wbm.scenergyspring.domain.post.jobPost.repository.JobPostApplyRepository;
 import com.wbm.scenergyspring.domain.post.jobPost.repository.JobPostRepository;
-import com.wbm.scenergyspring.domain.post.jobPost.service.Command.ApplyJobPostCommand;
-import com.wbm.scenergyspring.domain.post.jobPost.service.Command.BookMarkCommand;
-import com.wbm.scenergyspring.domain.post.jobPost.service.Command.CreateJobPostCommand;
-import com.wbm.scenergyspring.domain.post.jobPost.service.Command.DeleteJobPostCommand;
-import com.wbm.scenergyspring.domain.post.jobPost.service.Command.GetJobPostCommand;
+import com.wbm.scenergyspring.domain.post.jobPost.service.Command.*;
 import com.wbm.scenergyspring.domain.tag.entity.GenreTag;
 import com.wbm.scenergyspring.domain.tag.entity.InstrumentTag;
 import com.wbm.scenergyspring.domain.tag.entity.LocationTag;
@@ -38,8 +18,19 @@ import com.wbm.scenergyspring.domain.tag.repository.LocationTagRepository;
 import com.wbm.scenergyspring.domain.user.entity.Gender;
 import com.wbm.scenergyspring.domain.user.entity.User;
 import com.wbm.scenergyspring.domain.user.repository.UserRepository;
-
 import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JobPostServiceTest {
@@ -156,19 +147,22 @@ class JobPostServiceTest {
 		JobPost findJobPost = jobPostRepository.findById(id)
 			.orElseThrow(() -> new EntityNotFoundException("없는 게시글"));
 
-		Assertions.assertThat(id).isEqualTo(findJobPost.getId());
+		assertThat(id).isEqualTo(findJobPost.getId());
 
 		List<JobPostGenreTag> genreTagsInJobPost = findJobPost.getJobPostGenreTags();
-		Assertions.assertThat(genreTagsInJobPost).isNotNull();;
-		Assertions.assertThat(3).isEqualTo(genreTagsInJobPost.size());
+		assertThat(genreTagsInJobPost).isNotNull();
+		;
+		assertThat(3).isEqualTo(genreTagsInJobPost.size());
 
 		List<JobPostInstrumentTag> instrumentTagsInJobPost  = findJobPost.getJobPostInstrumentTags();
-		Assertions.assertThat(instrumentTagsInJobPost).isNotNull();;
-		Assertions.assertThat(3).isEqualTo(instrumentTagsInJobPost.size());
+		assertThat(instrumentTagsInJobPost).isNotNull();
+		;
+		assertThat(3).isEqualTo(instrumentTagsInJobPost.size());
 
 		List<JobPostLocationTag> locationTagInJobPost  = findJobPost.getJobPostLocationTags();
-		Assertions.assertThat(locationTagInJobPost).isNotNull();;
-		Assertions.assertThat(3).isEqualTo(locationTagInJobPost.size());
+		assertThat(locationTagInJobPost).isNotNull();
+		;
+		assertThat(3).isEqualTo(locationTagInJobPost.size());
 
 		jobPostRepository.deleteById(id);
 	}
@@ -257,29 +251,29 @@ class JobPostServiceTest {
 		boolean result = jobPostService.updateJobPost(id, updateRequest);
 
 		// then
-		Assertions.assertThat(result).isTrue();
+		assertThat(result).isTrue();
 
 		JobPost updatedJobPost = jobPostRepository.findById(id)
 			.orElseThrow(() -> new EntityNotFoundException("없는 게시글"));
 
-		Assertions.assertThat(updatedJobPost.getTitle()).isEqualTo("bbb");
-		Assertions.assertThat(updatedJobPost.getContent()).isEqualTo("bbb");
-		Assertions.assertThat(updatedJobPost.getExpirationDate()).isEqualTo(LocalDateTime.parse("2024-02-02T10:00:00"));
-		Assertions.assertThat(updatedJobPost.getPeopleRecruited()).isEqualTo(5L);
-		Assertions.assertThat(updatedJobPost.getBookMark()).isEqualTo(1L);
-		Assertions.assertThat(updatedJobPost.getIsActive()).isEqualTo(IsActive.active);
+		assertThat(updatedJobPost.getTitle()).isEqualTo("bbb");
+		assertThat(updatedJobPost.getContent()).isEqualTo("bbb");
+		assertThat(updatedJobPost.getExpirationDate()).isEqualTo(LocalDateTime.parse("2024-02-02T10:00:00"));
+		assertThat(updatedJobPost.getPeopleRecruited()).isEqualTo(5L);
+		assertThat(updatedJobPost.getBookMark()).isEqualTo(1L);
+		assertThat(updatedJobPost.getIsActive()).isEqualTo(IsActive.active);
 
 		List<JobPostGenreTag> updatedGenreTagsInJobPost = updatedJobPost.getJobPostGenreTags();
-		Assertions.assertThat(updatedGenreTagsInJobPost).isNotNull();
-		Assertions.assertThat(updatedGenreTagsInJobPost).hasSize(1);
+		assertThat(updatedGenreTagsInJobPost).isNotNull();
+		assertThat(updatedGenreTagsInJobPost).hasSize(1);
 
 		List<JobPostInstrumentTag> updatedInstrumentTagsInJobPost = updatedJobPost.getJobPostInstrumentTags();
-		Assertions.assertThat(updatedInstrumentTagsInJobPost).isNotNull();
-		Assertions.assertThat(updatedInstrumentTagsInJobPost).hasSize(1);
+		assertThat(updatedInstrumentTagsInJobPost).isNotNull();
+		assertThat(updatedInstrumentTagsInJobPost).hasSize(1);
 
 		List<JobPostLocationTag> updatedLocationTagsInJobPost = updatedJobPost.getJobPostLocationTags();
-		Assertions.assertThat(updatedLocationTagsInJobPost).isNotNull();
-		Assertions.assertThat(updatedLocationTagsInJobPost).hasSize(1);
+		assertThat(updatedLocationTagsInJobPost).isNotNull();
+		assertThat(updatedLocationTagsInJobPost).hasSize(1);
 
 		jobPostRepository.deleteById(id);
 	}
@@ -361,7 +355,7 @@ class JobPostServiceTest {
 		jobPostService.deleteJobPost(command2);
 
 		// then
-		Assertions.assertThat(now).isEqualTo(jobPostRepository.count());
+		assertThat(now).isEqualTo(jobPostRepository.count());
 	}
 
 	@Test
@@ -399,7 +393,7 @@ class JobPostServiceTest {
 			.orElseThrow(() -> new EntityNotFoundException("없는 게시글"));
 
 		// then
-		Assertions.assertThat(response.getTitle()).isEqualTo(findJob.getTitle());
+		assertThat(response.getTitle()).isEqualTo(findJob.getTitle());
 		jobPostRepository.deleteById(id);
 	}
 
@@ -419,7 +413,7 @@ class JobPostServiceTest {
 		List<GetJobPostCommandResponse> result = jobPostService.getMyJobPost(id);
 		int resultSize = result.size();
 		System.out.println(resultSize);
-		Assertions.assertThat(resultSize).isEqualTo(jobPostRepository.findAllByPost(id).size());
+		assertThat(resultSize).isEqualTo(jobPostRepository.findAllByPost(id).size());
 		userRepository.deleteById(id);
 	}
 
@@ -443,7 +437,7 @@ class JobPostServiceTest {
 		List<GetJobPostCommandResponse> result = jobPostService.getBookMarkJobPost(id);
 
 		//then
-		Assertions.assertThat(result.size()).isEqualTo(jobPostRepository.findAllBookMark(id).size());
+		assertThat(result.size()).isEqualTo(jobPostRepository.findAllBookMark(id).size());
 		userRepository.deleteById(id);
 
 	}
@@ -468,7 +462,7 @@ class JobPostServiceTest {
 		List<GetJobPostCommandResponse> result = jobPostService.getAllMyApply(id);
 
 		//then
-		Assertions.assertThat(result.size()).isEqualTo(jobPostRepository.findAllByPost(id).size());
+		assertThat(result.size()).isEqualTo(jobPostRepository.findAllByPost(id).size());
 		userRepository.deleteById(id);
 	}
 
@@ -481,7 +475,7 @@ class JobPostServiceTest {
 	void getAllJobPostList() {
 		List<GetJobPostCommandResponse> result = jobPostService.getAllJobPostList();
 		int resultSize = result.size();
-		Assertions.assertThat(resultSize).isEqualTo(jobPostRepository.count());
+		assertThat(resultSize).isEqualTo(jobPostRepository.count());
 	}
 
 	@Test
@@ -518,8 +512,8 @@ class JobPostServiceTest {
 		String result = jobPostService.ApplyJobPost(command);
 
 		// then
-		Assertions.assertThat("지원완료").isEqualTo(result);
-		Assertions.assertThat(1).isEqualTo(jobPostApplyRepository.count());
+		assertThat("지원완료").isEqualTo(result);
+		assertThat(1).isEqualTo(jobPostApplyRepository.count());
 
 		userRepository.deleteById(saveUser.getId());
 		jobPostRepository.deleteById(newJobPost.getId());
@@ -562,11 +556,116 @@ class JobPostServiceTest {
 		// then
 		String result = jobPostService.BookMarkJobPost(bookMarkCommand);
 
-		Assertions.assertThat(result).isEqualTo("북마크");
-		Assertions.assertThat(cnt + 1).isEqualTo(jobBookMarkRepository.count());
+		assertThat(result).isEqualTo("북마크");
+		assertThat(cnt + 1).isEqualTo(jobBookMarkRepository.count());
 
 		userRepository.deleteById(saveUser.getId());
 		jobPostRepository.deleteById(newJobPost.getId());
+	}
+
+	@Test
+	@DisplayName("구인구직 검색")
+	@Transactional
+	void searchJobPost() {
+		//given
+		List<InstrumentTag> instrumentTagList = new ArrayList<>();
+		InstrumentTag instrumentTag1 = saveInstrumentTag("기타");
+		InstrumentTag instrumentTag2 = saveInstrumentTag("드럼");
+		InstrumentTag instrumentTag3 = saveInstrumentTag("베이스");
+		instrumentTagList.add(instrumentTag1);
+		instrumentTagList.add(instrumentTag2);
+		instrumentTagList.add(instrumentTag3);
+
+		List<GenreTag> genreTagList = new ArrayList<>();
+		GenreTag genreTag1 = saveGenreTag("Jazz");
+		GenreTag genreTag2 = saveGenreTag("Pop");
+		GenreTag genreTag3 = saveGenreTag("Hiphop");
+		genreTagList.add(genreTag1);
+		genreTagList.add(genreTag2);
+		genreTagList.add(genreTag3);
+
+		List<LocationTag> locationTagList = new ArrayList<>();
+		LocationTag locationTag1 = saveLocationTag("서울");
+		LocationTag locationTag2 = saveLocationTag("인천");
+		LocationTag locationTag3 = saveLocationTag("대전");
+		locationTagList.add(locationTag1);
+		locationTagList.add(locationTag2);
+		locationTagList.add(locationTag3);
+
+
+		// given
+		User user = User.createNewUser(
+				"aaa@naver.com",
+				"aaaaa",
+				"aaaaa",
+				Gender.FEMALE,
+				"aaa"
+		);
+		User saveUser = userRepository.save(user);
+
+		JobPost newJobPost = JobPost.createNewJobPost(
+				user,
+				"aaa",
+				"aaaa",
+				LocalDateTime.parse("2024-02-01T10:00:00"),
+				4L,
+				0L,
+				IsActive.active
+		);
+
+		List<Long> genreTags = Arrays.asList(genreTag1.getId(), genreTag2.getId(), genreTag3.getId());
+		List<Long> instrumentTags = Arrays.asList(instrumentTag1.getId(), instrumentTag2.getId(), instrumentTag3.getId());
+		List<Long> locationTags = Arrays.asList(locationTag1.getId(), locationTag2.getId(), locationTag3.getId());
+
+		CreateJobPostCommand command = CreateJobPostCommand.builder()
+				.userId(saveUser.getId())
+				.title(newJobPost.getTitle())
+				.content(newJobPost.getContent())
+				.expirationDate(newJobPost.getExpirationDate())
+				.peopleRecruited(newJobPost.getPeopleRecruited())
+				.bookMark(newJobPost.getBookMark())
+				.isActive(newJobPost.getIsActive())
+				.locationTagIds(locationTags)
+				.genreTagIds(genreTags)
+				.instrumentTagIds(instrumentTags)
+				.build();
+		CreateJobPostCommand command1 = CreateJobPostCommand.builder()
+				.userId(saveUser.getId())
+				.title(newJobPost.getTitle())
+				.content(newJobPost.getContent())
+				.expirationDate(newJobPost.getExpirationDate())
+				.peopleRecruited(newJobPost.getPeopleRecruited())
+				.bookMark(newJobPost.getBookMark())
+				.isActive(newJobPost.getIsActive())
+				.locationTagIds(new ArrayList<>())
+				.genreTagIds(new ArrayList<>())
+				.instrumentTagIds(new ArrayList<>())
+				.build();
+
+		Long id = jobPostService.createJobPost(command);
+		Long id1 = jobPostService.createJobPost(command1);
+		//when
+		SearchAllJobPostCommand searchCommand1 = SearchAllJobPostCommand.builder()
+				.name(jobPostRepository.findById(id).get().getTitle())
+				.lt(null)
+				.gt(null)
+				.it(null)
+				.build();
+		SearchAllJobPostCommand searchCommand2 = SearchAllJobPostCommand.builder()
+				.name(null)
+				.lt(locationTags)
+				.gt(null)
+				.it(null)
+				.build();
+		System.out.println(jobPostRepository.findById(id).get().getTitle());
+		System.out.println(jobPostRepository.findById(id1).get().getTitle());
+		List<SearchAllJobPostResponse> result1 = jobPostService.searchAllJobPost(searchCommand1);
+		List<SearchAllJobPostResponse> result2 = jobPostService.searchAllJobPost(searchCommand2);
+		System.out.println("cnt : " + jobPostRepository.count());
+		System.out.println(searchCommand1.getName());
+		//then
+		assertThat(result1.size()).isEqualTo(2);
+		assertThat(result2.size()).isEqualTo(1);
 	}
 
 
