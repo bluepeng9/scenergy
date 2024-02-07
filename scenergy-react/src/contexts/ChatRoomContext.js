d885655306c17140aae7666c8d433519ed712475
김은지 <eungg0214@naver.com>
2024-02-07 22:32:43 +0900

    fix: exit chatroom ChatRoomList 실시간 렌더링 수정

--- scenergy-react/src/contexts/ChatRoomContext.js
+++ scenergy-react/src/contexts/ChatRoomContext.js
@@ -42,6 +42,26 @@ export const ChatRoomProvider = ({ children }) => {
     );
   };
 
+  const addChatRoom = (newRoom) => {
+    setChatRooms((prevChatRooms) => {
+      const isExist = prevChatRooms.some((room) => room.id === newRoom.id);
+      console.log(prevChatRooms);
+      console.log(newRoom.id);
+      if (!isExist) {
+        console.log(newRoom);
+        return [newRoom, ...prevChatRooms];
+      }
+      return prevChatRooms;
+    });
+  };
+
+  const removeChatRoom = (roomId) => {
+    console.log(chatRooms);
+    console.log(roomId);
+    setChatRooms((prevRooms) => prevRooms.filter((room) => room.id !== roomId));
+    console.log(chatRooms);
+  };
+
   const value = {
     selectedRoomId,
     setSelectedRoomId,
