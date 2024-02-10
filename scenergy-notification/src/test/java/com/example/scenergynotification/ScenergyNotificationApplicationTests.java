b671067d9a0d9d8e0864df18a3ac6ba3863a3ad9
bluepeng9 <bluepeng9@gmail.com>
2024-02-10 20:21:03 +0900

    fix: NotificationApi.js 서버 주소 변경

--- scenergy-notification/src/test/java/com/example/scenergynotification/ScenergyNotificationApplicationTests.java
+++ scenergy-notification/src/test/java/com/example/scenergynotification/ScenergyNotificationApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.scenergynotification;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ScenergyNotificationApplication.class)
 class ScenergyNotificationApplicationTests {
 
 	@Test
--- scenergy-react/src/apis/NotificationApi.js
+++ scenergy-react/src/apis/NotificationApi.js
@@ -2,11 +2,13 @@ class NotificationApi {
 
     #sse;
 
+    notificationUrl = process.env.REACT_APP_NOTIFICATION_API_URL
+
     connectToNotificationServer = (userId, onConnected, onNotification) => {
         if (this.#sse !== undefined) {
             return;
         }
-        this.#sse = new EventSource(`http://localhost:9001/connect/${userId}`);
+        this.#sse = new EventSource(`${this.notificationUrl}/${userId}`);
 
         this.#sse.addEventListener("notification", (event) => {
             onNotification(JSON.parse(event.data));
