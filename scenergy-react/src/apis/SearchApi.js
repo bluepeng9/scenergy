25e5920178b0aed43b7f4b4cfc28c7409e59f0d2
haeji <hjlove001@gmail.com>
2024-02-12 00:19:04 +0900

    fix: SearchApi export 수정

--- scenergy-react/src/apis/SearchApi.js
+++ scenergy-react/src/apis/SearchApi.js
@@ -1,9 +1,8 @@
 import ApiUtil from "./ApiUtil";
-import apiUtil from "./ApiUtil";
 
 class SearchApi {
   // 팔로잉하는 사용자들 중 검색
-  static searchFollowing = async (userId, word) => {
+  searchFollowing = async (userId, word) => {
     return await ApiUtil.post("/users/search-following", {
       userId: userId,
       word: word,
@@ -54,4 +53,5 @@ class SearchApi {
   };
 }
 
-export default SearchApi;
+const searchApi = new SearchApi();
+export default searchApi;
