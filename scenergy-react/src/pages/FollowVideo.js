c9cc6b5a5222e3bfb26b483219fcc54a007a0333
김은지 <eungg0214@naver.com>
2024-02-12 19:29:16 +0900

    feat: 홈화면 스크롤 스냅

--- scenergy-react/src/pages/FollowVideo.js
+++ scenergy-react/src/pages/FollowVideo.js
@@ -1,9 +1,18 @@
 import styles from "./FollowVideo.module.css";
 const FollowVideo = () => {
+  const dummyPosts = Array.from({ length: 10 }, (_, index) => ({
+    id: index,
+    title: `팔로우한 사용자의 게시물 ${index + 1}`,
+  }));
   return (
-    <>
-      <div className={styles["video-contain"]}></div>
-    </>
+    <div className={styles.followVideoContainer}>
+      {dummyPosts.map((post) => (
+        <div key={post.id} className={styles.videoContain}>
+          <h2>{post.title}</h2>
+          {/* 게시물 내용이나 이미지 등을 여기에 추가 */}
+        </div>
+      ))}
+    </div>
   );
 };
 
