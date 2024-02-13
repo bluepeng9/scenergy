a094a49b1118512fb0858d291819197b618350fa
haeji <hjlove001@gmail.com>
2024-02-13 15:41:02 +0900

    fix: SearchApi get 방식 수정

--- scenergy-react/src/apis/SearchApi.js
+++ scenergy-react/src/apis/SearchApi.js
@@ -19,37 +19,43 @@ class SearchApi {
   };
 
   searchUser = async (word) => {
-    try {
-      const response = await ApiUtil.get("/users/search", {
-        word: word,
-      });
-      const searchData = response.data;
-      if (searchData.success) {
-        return searchData.data;
-      } else {
-        throw new Error(searchData.message);
-      }
-    } catch (error) {
-      console.error("Error while searching user:", error);
-      throw error;
-    }
+    // try {
+    //   const response = await ApiUtil.get("/users/search", {
+    //     word: word,
+    //   });
+    //   const searchData = response.data;
+    //   if (searchData.success) {
+    //     return searchData.data;
+    //   } else {
+    //     throw new Error(searchData.message);
+    //   }
+    // } catch (error) {
+    //   console.error("Error while searching user:", error);
+    //   throw error;
+    // }
+    return await ApiUtil.get("/users/search", {
+      word: word,
+    });
   };
 
   searchFollowingAll = async (userId) => {
-    try {
-      const response = ApiUtil.get("/users/search-following", {
-        userId: userId,
-      });
-      const searchData = (await response).data;
-      if (searchData.success) {
-        return searchData.data;
-      } else {
-        throw new Error(searchData.message);
-      }
-    } catch (error) {
-      console.error("Error while searching following user", error);
-      throw error;
-    }
+    // try {
+    //   const response = ApiUtil.get("/users/search-following", {
+    //     userId: userId,
+    //   });
+    //   const searchData = (await response).data;
+    //   if (searchData.success) {
+    //     return searchData.data;
+    //   } else {
+    //     throw new Error(searchData.message);
+    //   }
+    // } catch (error) {
+    //   console.error("Error while searching following user", error);
+    //   throw error;
+    // }
+    return await ApiUtil.get("/users/search-following", {
+      userId: userId,
+    });
   };
 }
 
