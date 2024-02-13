d7ba473b0a5ab38c6032820b6cce1829efc9c8c1
bluepeng9 <bluepeng9@gmail.com>
2024-02-13 14:15:27 +0900

    feat: getToken 추가

--- scenergy-react/src/apis/ApiUtil.js
+++ scenergy-react/src/apis/ApiUtil.js
@@ -3,6 +3,22 @@ import axios from "axios";
 class ApiUtil {
   static BASE_URL = process.env.REACT_APP_API_URL
 
+
+    //토큰에서 userId 가져오기
+    static getUserIdFromToken = () => {
+        const token = this.getToken().split(" ")[1]; //Bearer 제거
+        if (!token) return null;
+
+        try {
+            const decode = jwtDecode(token);
+            console.log(decode);
+            return decode.sub; //id
+        } catch (error) {
+            console.error("토큰 티코드 안댐", error);
+            return null;
+        }
+    };
+
   static getToken = () => {
       return "Bearer " + localStorage.getItem("token") || "";
   };
