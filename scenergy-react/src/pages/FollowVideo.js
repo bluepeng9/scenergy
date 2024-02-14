3db5d61826ad5009dbfd5c301c0212c878283531
강대은 <eodms4334@naver.com>
2024-02-15 01:03:51 +0900

    feat: FollowVideo 완성

--- scenergy-react/src/pages/FollowVideo.js
+++ scenergy-react/src/pages/FollowVideo.js
@@ -2,11 +2,22 @@ import React, { useState, useEffect } from "react";
 import styles from "./FollowVideo.module.css";
 import videoPostApi from "../apis/VideoPostApi";
 import ApiUtil from "../apis/ApiUtil";
+import musicTitle from "../assets/musicTitle.png";
+import artist from "../assets/artist.png";
+import profileLink from "../assets/basic_profile.png";
+import { useNavigate } from "react-router-dom";
 
 const FollowVideo = () => {
   // API에서 받아온 영상 데이터를 저장할 상태
   console.log("홈화면 1");
   const [followingVideos, setFollowingVideos] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleProfileClick = (userId) => {
+    // 프로필 페이지로 이동하는 코드
+    navigate(`/profile/${userId}`);
+  };
 
   useEffect(() => {
     // API 호출 함수를 정의
@@ -38,9 +49,16 @@ const FollowVideo = () => {
       {/* 영상 데이터를 반복하여 화면에 렌더링 */}
       {followingVideos.map((item) => (
         <div key={item.id} className={styles.videoContain}>
-          <div className={styles.videoDiv}>
+          <div className={`${styles.videoDiv} `}>
             {/* 왼쪽에 동영상 */}
             <div className={styles.videoContent}>
+              <div
+                className={`${styles.profileLink}`}
+                onClick={() => handleProfileClick(item.userId)}
+              >
+                <img src={profileLink} alt="Profile Link" width="50" />
+              </div>
+              <p>{`user ${item.userId}`}</p>
               <video controls width="450">
                 <source src={item.video.videoUrlPath} type="video/mp4" />
                 Your browser does not support the video tag.
@@ -49,13 +67,23 @@ const FollowVideo = () => {
 
             {/* 오른쪽에 영상 정보 */}
             <div className={styles.videoInfo}>
-              <h2>{item.title}</h2> {/*제목*/}
-              <hr />
-              <p>{item.content}</p> {/*상세내용*/}
-              <h2 className={styles.videoHr}>곡 정보</h2>
-              <hr />
-              <p>{item.video.artist}</p>
-              <p>{item.video.musicTitle}</p>
+              <div className={styles.titleContent}>
+                <h2>{item.title}</h2> {/*제목*/}
+                <hr />
+                <p className={styles.content}>{item.content}</p> {/*상세내용*/}
+              </div>
+              <div className={styles.musicTieleArtist}>
+                <h2 className={styles.videoHr}>곡 정보</h2>
+                <hr />
+                <div className={styles.musicTitle}>
+                  <img src={musicTitle} alt="Music Title" width="30" />
+                  <p>{item.video.artist}</p>
+                </div>
+                <div className={styles.artist}>
+                  <img src={artist} alt="Artist" width="30" />
+                  <p>{item.video.musicTitle}</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
