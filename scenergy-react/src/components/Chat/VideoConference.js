41c32eb033c4f14430120164943924df3a6b3b99
imin <lllgee@naver.com>
2024-02-14 14:35:21 +0900

    feat: peer-js 방 이동 or 버튼 클릭 시 종료 기능 추가

--- scenergy-react/src/components/Chat/VideoConference.js
+++ scenergy-react/src/components/Chat/VideoConference.js
@@ -1,24 +1,18 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import Peer from 'peerjs';
 import styles from "./ChatRoomReal.module.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faVideo} from "@fortawesome/free-solid-svg-icons";
 
-
-const VideoConference = ({chatRoomId, chatRoomUsers, chatRoomUsersSeq, userId, connectUserId}) => {
-    const [peerId, setPeerId] = useState('');
-    const [remotePeerIdValue, setRemotePeerIdValue] = useState('');
+const VideoConference = ({chatRoomId, userId, connectUserId}) => {
     const remoteVideoRef = useRef(null);
     const currentUserVideoRef = useRef(null);
     const peerInstance = useRef(null);
+    const callRef = useRef(null);
 
     useEffect(() => {
         console.log(connectUserId)
         const peer = new Peer("" + chatRoomId + userId + "");
-
-        peer.on('open', (peerId) => {
-            setPeerId(peerId)
-        });
 
         peer.on('call', (call) => {
             var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
@@ -39,23 +33,32 @@ const VideoConference = ({chatRoomId, chatRoomUsers, chatRoomUsersSeq, userId, c
                     }
                 });
             });
+            callRef.current = call; // call 객체 저장
         })
 
         peerInstance.current = peer;
-    }, [connectUserId])
 
-    const call = (remotePeerId) => {
+        return () => {
+            hangUp()
+        };
+
+    }, [chatRoomId, userId, connectUserId]);
+
+    const call = async (remotePeerId) => {
         var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-        getUserMedia({video: true, audio: true}, (mediaStream) => {
+        try {
+            const mediaStream = await new Promise((resolve, reject) => {
+                getUserMedia({video: true, audio: true}, resolve, reject);
+            });
 
             currentUserVideoRef.current.srcObject = mediaStream;
             currentUserVideoRef.current.play();
 
-            const call = peerInstance.current.call(remotePeerId, mediaStream)
+            const call = peerInstance.current.call(remotePeerId, mediaStream);
 
             call.on('stream', (remoteStream) => {
-                remoteVideoRef.current.srcObject = remoteStream
+                remoteVideoRef.current.srcObject = remoteStream;
                 var playPromise = remoteVideoRef.current.play();
 
                 if (playPromise !== undefined) {
@@ -64,15 +67,29 @@ const VideoConference = ({chatRoomId, chatRoomUsers, chatRoomUsersSeq, userId, c
                         .catch(error => {
                         });
                 }
-
             });
-        });
-    }
+            callRef.current = call; // call 객체 저장
+        } catch (error) {
+            console.error('Error accessing media devices:', error);
+        }
+    };
+
+    const hangUp = () => {
+        if (callRef.current) {
+            callRef.current.close(); // 연결된 비디오 중지
+        }
+        // 비디오 스트림 초기화
+        currentUserVideoRef.current.srcObject = null;
+        remoteVideoRef.current.srcObject = null;
+    };
 
     return (
         <div>
             <div className={styles.doRtc} onClick={() => call("" + chatRoomId + connectUserId + "")}>
                 <FontAwesomeIcon icon={faVideo}/>
+            </div>
+            <div className={styles.doRtc} onClick={hangUp}>
+                <FontAwesomeIcon icon={faVideo}/> 종료
             </div>
             <div>
                 <video ref={currentUserVideoRef}/>
@@ -82,6 +99,6 @@ const VideoConference = ({chatRoomId, chatRoomUsers, chatRoomUsersSeq, userId, c
             </div>
         </div>
     );
-}
+};
 
 export default VideoConference;
