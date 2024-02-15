eb541f41b62e213f4d111f4e061b69b2cf7643f6
imin <lllgee@naver.com>
2024-02-15 22:12:52 +0900

    feat: navbar & 색상 수정

--- scenergy-react/src/components/commons/Navbar/Navbar.js
+++ scenergy-react/src/components/commons/Navbar/Navbar.js
@@ -1,15 +1,20 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import styles from "./Navbar.module.css";
-import {useState} from "react";
+import React, {useState} from "react";
 import ApiUtil from "../../../apis/ApiUtil";
+import {useCookies} from "react-cookie";
 
 const Navbar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const navigate = useNavigate();
   const isLoggedIn = !!ApiUtil.getUserIdFromToken(); // 토큰을 가지고 있는지 여부로 로그인 상태 판단
-
+  const location = useLocation();
+  const {pathname} = location;
   const homePath = isLoggedIn ? "/home2" : "/home"; // 로그인 상태에 따라 다른 홈 경로 선택
+  const pathSegments = pathname.split("/");
 
+  const routerName = pathSegments[pathSegments.length - 1];
   // notificationApi.connectToNotificationServer(2, (data) => {
   //   console.log(data);
   // }, (data) => {
@@ -19,23 +24,30 @@ const Navbar = () => {
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
+
+  const handleLogout = () => {
+    removeCookie("accessToken");
+    localStorage.removeItem("token"); //토큰 제거
+    navigate("/home");
+  };
+
   return (
-    <nav className={styles.NavBarContainer}>
-      <ul>
-        <li className={styles.ScenergyTab}>
-            <Link to="/" style={{fontWeight: 'bold', fontSize: '25px'}}>Scene:rgy</Link>
-        </li>
-        <li>
-          <Link to={homePath}>홈</Link>
-        </li>
-        <li>
-          <Link to="/search">검색</Link>
-        </li>
-        <li
-          className={`${styles.NavBarDropdownGlobal} ${dropdownOpen ? styles.dropdownOpen : ""}`}
-          onMouseEnter={toggleDropdown}
-          onMouseLeave={toggleDropdown}
-        >
+      <nav className={styles.NavBarContainer}>
+        <ul>
+          <li className={styles.ScenergyTab}>
+            <Link to="/" style={{fontWeight: 'bold', fontSize: '25px', color: '#000000'}}>scene:rgy</Link>
+          </li>
+          <li>
+            <Link to={homePath}>홈</Link>
+          </li>
+          <li>
+            <Link to="/search">검색</Link>
+          </li>
+          <li
+              className={`${styles.NavBarDropdownGlobal} ${dropdownOpen ? styles.dropdownOpen : ""}`}
+              onMouseEnter={toggleDropdown}
+              onMouseLeave={toggleDropdown}
+          >
             {/*<Link to="/scenergy" className={styles.NavScenergy}>*/}
             {/*  시너지*/}
             {/*</Link>*/}
@@ -53,21 +65,24 @@ const Navbar = () => {
             {/*    <Link to="/scenergy/bookmark">북마크</Link>*/}
             {/*  </li>*/}
             {/*</ul>*/}
-        </li>
-        <li>
-          <Link to="/videoupload">업로드</Link>
-        </li>
-        <li className={styles.NavBarNotice}>
-          <Link to="/notification">알림</Link>
-        </li>
-        <li>
-          <Link to="/chat">메세지</Link>
-        </li>
-        <li>
-          <Link to={`/profile/${ApiUtil.getUserIdFromToken()}`}>프로필</Link>
-        </li>
-      </ul>
-    </nav>
+          </li>
+          <li>
+            <Link to="/videoupload">업로드</Link>
+          </li>
+          <li className={styles.NavBarNotice}>
+            <Link to="/notification">알림</Link>
+          </li>
+          <li>
+            <Link to="/chat">메세지</Link>
+          </li>
+          <li>
+            <Link to={`/profile/${ApiUtil.getUserIdFromToken()}`}>프로필</Link>
+          </li>
+        </ul>
+        <button onClick={handleLogout} className={styles["logout"]}
+                hidden={(routerName !== "home2") && (routerName !== "home")}>logout
+        </button>
+      </nav>
   );
 };
 
--- scenergy-react/src/pages/Home2Page.js
+++ scenergy-react/src/pages/Home2Page.js
@@ -1,32 +1,17 @@
 // Home.js (로그인 됐을때 화면)
 import React from "react";
-import {useCookies} from "react-cookie";
-import {useNavigate} from "react-router-dom";
 import Navbar from "../components/commons/Navbar/Navbar";
 import styles from "./Home2Page.module.css";
 import FollowVideo from "./FollowVideo";
 
 const Home2Page = () => {
-  const [cookies, setCookie, removeCookie] = useCookies();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    // 로그아웃 시 쿠키에서 토큰 제거
-    removeCookie("accessToken");
-    localStorage.removeItem("token"); //토큰 제거
-    // 로그인 화면으로 이동
-    navigate("/home");
-  };
 
   return (
     <>
       <div className={styles["content"]}>
         <div className={styles["navbar"]}>
           <Navbar/>
-          <div className={styles["logout-div"]}>
-            <button onClick={handleLogout}>로그아웃</button>
-            {/* 로그아웃 버튼 */}
-          </div>
+
         </div>
         <div className={styles["video"]}>
           <FollowVideo></FollowVideo>
