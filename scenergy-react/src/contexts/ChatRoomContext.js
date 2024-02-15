2fba81deb4c40a1a3b0d5d46e6cd91e6177d3024
김은지 <eungg0214@naver.com>
2024-02-15 22:50:55 +0900

    feat:rtc 화면 ui

--- scenergy-react/src/contexts/ChatRoomContext.js
+++ scenergy-react/src/contexts/ChatRoomContext.js
@@ -42,26 +42,6 @@ export const ChatRoomProvider = ({ children }) => {
     );
   };
 
-  const addChatRoom = (newRoom) => {
-    setChatRooms((prevChatRooms) => {
-      const isExist = prevChatRooms.some((room) => room.id === newRoom.id);
-      console.log(prevChatRooms);
-      console.log(newRoom.id);
-      if (!isExist) {
-        console.log(newRoom);
-        return [newRoom, ...prevChatRooms];
-      }
-      return prevChatRooms;
-    });
-  };
-
-  const removeChatRoom = (roomId) => {
-    console.log(chatRooms);
-    console.log(roomId);
-    setChatRooms((prevRooms) => prevRooms.filter((room) => room.id !== roomId));
-    console.log(chatRooms);
-  };
-
   const value = {
     selectedRoomId,
     setSelectedRoomId,
